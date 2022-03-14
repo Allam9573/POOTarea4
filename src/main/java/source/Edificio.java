0c7daf2c15297f2a995897ab1742c690806a54fe
Allam9573 <aallamdaniel@gmail.com>
2022-03-13 23:01:25 -0600

    primer commit

---
+++ src/main/java/source/Edificio.java
@@ -0,0 +1,11 @@
+package source;
+
+public class Edificio {
+	
+	private String tipoInstalacion;
+	private double ancho;
+	private double largo;
+	private int numeroPisos;
+	
+
+}
---
+++ src/main/java/source/Polideportivo.java
@@ -0,0 +1,9 @@
+package source;
+
+public class Polideportivo {
+	private int numeroOficinas;
+	private double ancho;
+	private double largo;
+	private int numeroPisos;
+	
+}
